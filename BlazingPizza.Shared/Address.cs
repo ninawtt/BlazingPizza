d3e64be605ecd5da3bfa17cd7454c791c78bcd0e
Nina <ninawu424@gmail.com>
2021-06-26 16:51:43 -0400

    Added checkout functionality

--- BlazingPizza.Shared/Address.cs
+++ BlazingPizza.Shared/Address.cs
@@ -6,16 +6,22 @@ namespace BlazingPizza
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter your name."), MaxLength(100)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter Line1."), MaxLength(100)]
         public string Line1 { get; set; }
 
+        [MaxLength(100)]
         public string Line2 { get; set; }
 
+        [Required(ErrorMessage = "Please enter City."), MaxLength(50)]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "Please enter Region."), MaxLength(20)]
         public string Region { get; set; }
 
+        [Required(ErrorMessage = "Please enter Postal Code."), MaxLength(20)]
         public string PostalCode { get; set; }
     }
 }
